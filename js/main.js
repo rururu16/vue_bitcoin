7622a8424a656e8c77680230d6356b547d259a31
rururu16 <ynk618172@gmail.com>
2018-12-18 03:30:04 +0900

    add api

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 var app = new Vue ({
   el: '#app',
-  date: {
+  data: {
     bpi: null
   },
   mounted: function() {
-    
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(function(response){
+      this.bpi = response.data.bpi
+    }.bind(this))
+    .catch(function(error){
+      console.log(error)
+    })
   }
-
 })
